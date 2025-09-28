5bbaa3dd3f380de29abed3a8db2f0036176093c0
VictorCabria11 <victor.cabria@bexsoluciones.com>
2025-09-28 17:23:30 -0500

    Add models, factories, and migrations for categories, questions, answers, comments, and hearts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Category;
+use App\Models\Question;
 use App\Models\User;
+use App\Models\Answer;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +16,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(19)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $categorias = Category::factory(4)->create();
+
+        $questions = Question::factory(50)->create(
+            [
+                'category_id' => fn() => $categorias->random()->id,
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+            ]
+        
+        );
+        $answers = Answer::factory(50)->create(
+            [
+                'question_id' => fn() => $questions->random()->id,
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+            ]);
+
+        Comment::factory(100)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id' => fn() => $answers->random()->id,
+            'commentable_type' =>  Answer::class,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id' => fn() => $questions->random()->id,
+            'commentable_type' =>  Question::class,
         ]);
     }
 }
